ed265921f6f8da63a97762c301f08d2c8b268b6f
ssr <ssr1282185158@gmail.com>
2019-08-20 16:57:33 +0800

    ssr

---
+++ Jdbc/src/com/ssr/test.java
@@ -0,0 +1,23 @@
+package com.ssr;
+
+import java.sql.*;
+
+/**
+ * @author ：susr
+ * @date ：2019/8/17 18:10
+ * @version: 1.0
+ * @description：TODO
+ */
+public class test {
+    public static void main(String[] args) throws SQLException {
+
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/ssr","root","root");
+        String sql = "update world set name = 'xjp' where id = 1";
+        Statement statement = connection.createStatement();
+        int i = statement.executeUpdate(sql);
+        System.out.println(i);
+        statement.close();
+        connection.close();
+
+    }
+}
